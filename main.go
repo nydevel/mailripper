cf67c445484d4abdfe4b9ff3c56b5f01a8a41c3a
Nikolay Yasinskiy <1@shfu.ru>
2020-06-14 09:53:19 +0500

    Work in progress

--- main.go
+++ main.go
@@ -11,11 +11,13 @@ import (
 var paramIP string
 var paramPort int
 var paramTimeout int
+var paramUsername string
 
 func init() {
 	flag.StringVar(&paramIP, "ip", "127.0.0.1", "Please set ip address for connection, default is 127.0.0.1")
 	flag.IntVar(&paramPort, "port", 25, "Default port is 25")
 	flag.IntVar(&paramTimeout, "timeout", 500, "Timeout in ms")
+	flag.StringVar(&paramUsername, "username", "user", "Default username is user")
 }
 
 func main() {
@@ -29,5 +31,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Port is open, lets try to send VRFY command")
+	fmt.Fprintf(conn, "VRFY %s", paramUsername)
 	conn.Close()
 }

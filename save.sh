8fe3f3cdc2016757c2abca3cf72c22987af84038
Nikolay Yasinskiy <1@shfu.ru>
2020-06-14 10:31:10 +0500

    Changes

--- save.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'We start to commit all changes and send them to git repository'
-
-git add --all
-git commit -a -m 'new changes'
-git push
-
-echo 'Your changes was send to git'

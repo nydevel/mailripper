3e6c513df0b74dfe07105570b1922f59e75f589f
Nikolay Yasinskiy <1@shfu.ru>
2020-06-14 11:00:09 +0500

    new changes

--- main.go
+++ main.go
@@ -11,13 +11,11 @@ import (
 
 var paramIP string
 var paramPort int
-var paramTimeout int
 var paramFile string
 
 func init() {
 	flag.StringVar(&paramIP, "ip", "127.0.0.1", "Please set ip address for connection, default is 127.0.0.1")
 	flag.IntVar(&paramPort, "port", 25, "Default port is 25")
-	flag.IntVar(&paramTimeout, "timeout", 500, "Timeout in ms")
 	flag.StringVar(&paramFile, "file", "file", "Filename with usernames")
 }
 
